1cc2e939774b81a910c1a4e713162b0b0f9c6152
hrupesh <rcashok0@gmail.com>
2020-08-26 02:34:58 +0530

    Auto Commit �

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+const messages = [];
+
+const typeDefs = `
+    type Message {
+        id: ID!
+        user: String!
+        content: String!
+    }
+
+    type Query {
+        messages: [Message!]
+    }
+
+    type Mutation{
+        postMessage(user: String!, constent: String!): ID!
+    }
+`;
+
+const resolvers = {
+  Query: {
+    messages: () => messages,
+  },
+  Mutation:{
+      postMessage:(parent, {user, content}) => {
+          const id = messages.length;
+          
+      }
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(({ port }) => {
+  console.log(`Serving on  http://localhost:${port}/`);
+});
