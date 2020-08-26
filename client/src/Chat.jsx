ae087915e2839765ad78394404b4d035d2f56c34
hrupesh <rcashok0@gmail.com>
2020-08-26 17:19:38 +0530

    Auto Commit �

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
+
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      user
+      content
+    }
+  }
+`;
 
 const Chat = () => {
   return <h6>This is a chat window</h6>;
