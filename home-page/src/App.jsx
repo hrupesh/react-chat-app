91c3fb1b05ae91ddc8459109a3a090609a9185c7
hrupesh <rcashok0@gmail.com>
2020-08-28 02:18:45 +0530

    Auto Commit �

--- home-page/src/App.jsx
+++ home-page/src/App.jsx
@@ -31,7 +31,7 @@ const App = () => (
       </NavbarBrand>
       <NavbarToggler />
     </Navbar>
-    <Container className="my-4 cstm-nmrc">
+    <Container className="cstm-nmrc">
       <h4 className="text-center">Start Chatting to Random Strangers</h4>
       <Chat />
     </Container>
