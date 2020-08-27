ceeec4cc312da14e96778831adebcb369babf7c5
hrupesh <rcashok0@gmail.com>
2020-08-27 19:52:12 +0530

    Auto Commit �

--- home-page/src/App.jsx
+++ home-page/src/App.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Container } from "shards-react";
+import {
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  FormInput,
+  Collapse,
+} from "shards-react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
@@ -10,10 +27,52 @@ import "./index.css";
 import Chat from "chat/Chat";
 
 const App = () => (
-  <Container>
-    <h1 className="display-3">Chat!</h1>
-    <Chat />
-  </Container>
+  <>
+    <Navbar type="dark" theme="primary" expand="md">
+      <NavbarBrand href="#">Shards React</NavbarBrand>
+      <NavbarToggler onClick={this.toggleNavbar} />
+
+      <Collapse open={this.state.collapseOpen} navbar>
+        <Nav navbar>
+          <NavItem>
+            <NavLink active href="#">
+              Active
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#" disabled>
+              Disabled
+            </NavLink>
+          </NavItem>
+          <Dropdown open={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+            <DropdownToggle nav caret>
+              Dropdown
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>Action</DropdownItem>
+              <DropdownItem>Another action</DropdownItem>
+              <DropdownItem>Something else here</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </Nav>
+
+        <Nav navbar className="ml-auto">
+          <InputGroup size="sm" seamless>
+            <InputGroupAddon type="prepend">
+              <InputGroupText>
+                <FontAwesomeIcon icon={faSearch} />
+              </InputGroupText>
+            </InputGroupAddon>
+            <FormInput className="border-0" placeholder="Search..." />
+          </InputGroup>
+        </Nav>
+      </Collapse>
+    </Navbar>
+    <Container>
+      <h1 className="display-3">Chat!</h1>
+      <Chat />
+    </Container>
+  </>
 );
 
 ReactDOM.render(<App />, document.getElementById("app"));
