088f450d309b05c500f866d4916232c8a1c63c4d
hrupesh <rcashok0@gmail.com>
2020-08-27 19:43:18 +0530

    Auto Commit �

--- home-page/src/App.jsx
+++ home-page/src/App.jsx
@@ -11,18 +11,8 @@ import Chat from "chat/Chat";
 
 const App = () => (
   <Container>
-    <p className="h6">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam itaque
-      excepturi vitae fuga? Beatae facilis illum quos ipsa, ea amet saepe
-      excepturi ducimus? Quam aliquam delectus a ab vitae laudantium.{" "}
-    </p>
     <h1 className="display-3">Chat!</h1>
     <Chat />
-    <p className="h6">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae,
-      laudantium iusto quos optio odio nobis. Atque sequi, velit placeat
-      consectetur iste nemo dicta tempore amet. Non facere ut saepe nulla.{" "}
-    </p>
   </Container>
 );
 
