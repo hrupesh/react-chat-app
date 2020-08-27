acf1e829b78815a08c35892fc64a0cafcbccb297
hrupesh <rcashok0@gmail.com>
2020-08-27 17:42:16 +0530

    Auto Commit �

--- home-page/webpack.config.js
+++ home-page/webpack.config.js
@@ -3,7 +3,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 
 module.exports = {
   output: {
-    publicPath: "http://localhost:8080/",
+    publicPath: "http://localhost:8081/",
   },
 
   resolve: {
@@ -11,7 +11,7 @@ module.exports = {
   },
 
   devServer: {
-    port: 8080,
+    port: 8081,
   },
 
   module: {
@@ -32,7 +32,8 @@ module.exports = {
 
   plugins: [
     new ModuleFederationPlugin({
-      name: "starter",
+      name: "home",
+      library: { type: "var", name: "home" },
       filename: "remoteEntry.js",
       remotes: {},
       exposes: {},
